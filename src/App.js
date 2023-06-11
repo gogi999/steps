c5934e094ab63bfea4810a521409d1ef0abd081b
gogi999 <g.petrovic9@gmail.com>
2023-06-11 17:40:16 +0200

    Completed steps app

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+const Steps = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test, setTest] = useState({ name: 'Jonas' });
+
+  const handlePrevious = () => {
+    if (step > 1) setStep((prevStep) => prevStep - 1);
+  }
+
+  const handleNext = () => {
+    if (step < 3) setStep((nextStep) => nextStep + 1);
+
+    // Bad practice
+    // test.name = 'Fred';
+
+    // Wright way 
+    // setTest({ name: 'Fred' });
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <button 
+              style={{ backgroundColor: "#7950f2", color: "#fff" }} 
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button 
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
